d81af3a1742c691d767910ed4e1371bd92eb0cf8
kashafhafeez <kashafhafeez6@gmail.com>
2026-04-27 16:21:28 +0500

    C++ Learning

---
+++ class02.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	/* int a =5;
+	int b = 10;
+	int d =a + b;
+	cout << "my Result is: " << d << "\n\n "; */
+	
+	/* //find area of a circle 
+	int c = 20;
+	float a = 3.14 * c * c;
+	
+	cout << "area of a circle is : "<< a<<endl; */
+	
+	//find average ?
+	/*float a = 10;
+	float b = 20;
+	float c = 13;
+	float d = a+ b + c / 3;
+	cout <<"Average of 3 number is: "<< d<< "\n\n"; */
+	
+	/*int a = 10;
+	int b = 20;
+	int c = 1;
+	int add = a + b;
+	int cal = add * c /2;
+	cout<< "my result is: "<<cal<<"\n\n";*/
+	
+	//user input
+	int a , b , c;
+	cout << "Enter first number ";
+	cin >> a;
+	cout << "Enter second number ";
+	cin >> b;
+	c = a + b;
+	cout << "Addition is: "<< c <<"\n\n";
+	
+	 
+	
+}
